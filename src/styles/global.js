3a4574c45e0eb5b93004fea597b317f26579c1b1
vctrhugoop <vhdestroy@hotmail.com>
2024-03-30 09:13:32 -0300

    🌈 style: add  theme and global

---
+++ src/styles/global.js
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    font-size: 62.5%;
+  }
+
+  body {
+    background: ${({ theme }) => theme.COLORS.DARK['400']};
+    color: ${({ theme }) => theme.COLORS.LIGHT['100']};
+
+    font-size: 1.6rem;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button, textarea {
+    font-family: ${({ theme }) => theme.FONTS.TITLE};
+    outline: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button, a {
+    cursor: pointer;
+    transform: filter .2s;
+  }
+
+  button:hover, a:hover {
+    filter: brightness(.9);
+  }
+`;
