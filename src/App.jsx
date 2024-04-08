40f8baa3d0d42cd04e1c812f932acbfdd34347c8
vctrhugoop <vhdestroy@hotmail.com>
2024-04-08 20:56:23 -0300

    ✨ feat: add providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { Toaster } from 'sonner';
 import { Routes } from './routes';
 
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
 
+import { AuthProvider } from './hooks/auth';
+
 export function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+      <Toaster
+        richColors
+        position='top-right'
+        toastOptions={{
+          className: 'my-toast',
+        }}
+      />
     </ThemeProvider>
   );
 }
