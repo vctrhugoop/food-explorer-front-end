0700ee56ed635ad92149900f74928a3020793417
vctrhugoop <vhdestroy@hotmail.com>
2024-04-08 21:01:57 -0300

    ✨ feat: save user login in local storege

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 import { api } from '../services/api';
 
 const AuthContext = createContext({});
@@ -10,6 +10,9 @@ export function AuthProvider({ children }) {
     try {
       const response = await api.post('/sessions', { email, password });
       const { user, token } = response.data;
+
+      localStorage.setItem('@foodexplorer:user', JSON.stringify(user));
+      localStorage.setItem('@foodexplorer:token', token);
 
       api.defaults.headers.authorization = `Bearer ${token}`;
 
@@ -23,6 +26,20 @@ export function AuthProvider({ children }) {
       }
     }
   }
+
+  useEffect(() => {
+    const token = localStorage.getItem('@foodexplorer:token');
+    const user = localStorage.getItem('@foodexplorer:user');
+
+    if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
+      setDate({
+        token,
+        user: JSON.parse(user),
+      });
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
