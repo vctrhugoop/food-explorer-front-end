cab33af35e3ba8296cdbd4553513acd4df41a13e
vctrhugoop <vhdestroy@hotmail.com>
2024-04-12 18:50:52 -0300

    ✨ feat: create new page and add route nav

---
+++ src/pages/NewDish/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { NewDishContainer } from './styles';
+
+export function NewDish() {
+  return <NewDishContainer>oi</NewDishContainer>;
+}
--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 
 import { DefaultLayout } from '../layout/DefaultLayout';
 import { Home } from '../pages/Home';
+import { NewDish } from '../pages/NewDish';
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path='/' element={<DefaultLayout />}>
         <Route path='/' element={<Home />} />
+        <Route path='/new-dish' element={<NewDish />} />
       </Route>
     </Routes>
   );
